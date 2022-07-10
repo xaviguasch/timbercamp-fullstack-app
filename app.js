fc821859422680efe46d41ca63673c08848c6b59
xaviguasch <xaviguasch@gmail.com>
2022-07-10 22:51:46 +0200

    connected app to mongo

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 
+const Campground = require('./models/campground')
+
+mongoose
+  .connect('mongodb://localhost:27017/timber-camp')
+  .then((result) => {
+    console.log('mongo connection open!')
+  })
+  .catch((error) => {
+    console.log('Oh no mongo connection error!!!')
+    console.log(error)
+  })
+
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -9,6 +21,12 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+app.get('/makecampground', async (req, res) => {
+  const camp = new Campground({ title: 'My Backyard' })
+  await camp.save()
+
+  res.send(camp)
 })
 
 app.listen(3000, () => {
