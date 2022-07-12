eab97ae140c26dc6dcea509e61405090737b78fb
xaviguasch <xaviguasch@gmail.com>
2022-07-12 12:28:02 +0200

    added ejs-mate package for layout templates

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
+const ejsMate = require('ejs-mate')
 
 const Campground = require('./models/campground')
 
@@ -17,6 +18,7 @@ mongoose
 
 const app = express()
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
